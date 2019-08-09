911aa177cd04efe98f4b77ce6e651c3b6489f395
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-08-09 11:58:16 -0400

    axios

--- tacobell/tacobell.ts
+++ tacobell/tacobell.ts
@@ -14,6 +14,16 @@ export function removeZeroes(tuples) {
     const output = [];
     tuples.forEach((tuple) => {
         if (tuple[1] !== 0) output.push(tuple);
-        });
+    });
     return output;
+}
+
+export function take(n, tuples) {
+    return tuples.slice(0, n);
+}
+
+export function compose(...funcs) {
+    let result = null;
+    funcs.forEach((fn) => result = fn(result));
+    return result;
 }
--- tests/tacobell.spec.ts
+++ tests/tacobell.spec.ts
@@ -1,7 +1,10 @@
-
-import {removeZeroes, sortAscending} from '../tacobell/tacobell';
+import {removeZeroes, sortAscending, take} from '../tacobell/tacobell';
 import {expect} from 'chai';
 import 'mocha';
+// npm install @types/node --save-de
+const axios = require('axios');
+
+
 
 // Ref: https://medium.com/building-ibotta/testing-arrays-and-objects-with-chai-js-4b372310fe6d
 describe('sort ascending', () => {
@@ -36,5 +39,38 @@ describe('remove zeroes', () => {
         ];
 
         expect(removeZeroes(orig)).to.eql(expected);
+    });
+});
+
+
+describe('take first n', () => {
+
+    it('returns an array of first n', () => {
+        const orig = [
+            ["blue", 1],
+            ["red", 0],
+            ["green", 2]
+        ];
+        const expected = [
+            ["blue", 1],
+            ["red", 0],
+        ];
+
+        expect(take(2, orig)).to.eql(expected);
+    });
+});
+
+
+describe('Test Async', () => {
+
+    it('axios get example', (done) => {
+        const expected = 'mce9 aipiggybot\n';
+        let url = "https://aipiggybot.io/";
+        axios.get(url).then(response => {
+            console.log(response.data);
+            expect(response.data).to.equal(expected);
+            done();
+        });
+
     });
 });
